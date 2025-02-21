38de57c4275179e62b5919f6de573ac7726aa909
Rege-97 <cogus1027@gmail.com>
2025-02-21 17:16:08 +0900

    ✅ Test: 앞에 말이 있을 때 상대 말 못잡는 오류 수정

--- Test/KCH/src/moveTest/Pawn.java
+++ Test/KCH/src/moveTest/Pawn.java
@@ -38,11 +38,90 @@ public class Pawn extends JButton {
 	public void BlackMove(JPanel boards[][], JButton movepins[][], JPanel p_board, Pawn pawns[]) {
 		Pawn pawn = this;
 
+		boolean attack = false;
+
 		if (row + 1 > 8) {
 			return;
 
 		} else {
-			if (boards[row + 1][col].getComponentCount() == 2) {
+			removeAction(movepins);
+
+			// 왼쪽 대각선
+			if (row + 1 <= 8 && col - 1 >= 1) {
+				if (boards[row + 1][col - 1].getComponentCount() == 2) {
+					if (((Pawn) boards[row + 1][col - 1].getComponent(1)).side.equals("white")) {
+
+						movepins[row + 1][col - 1].setVisible(true);
+						attack = true;
+
+						movepins[row + 1][col - 1].addActionListener(new ActionListener() {
+
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								boards[row][col].remove(pawn);
+
+								boards[row + 1][col - 1].add(pawn);
+								pawn.setBackground(Color.white);
+
+								movepinsNotVisible(movepins);
+
+								boards[row + 1][col - 1].remove(boards[row + 1][col - 1].getComponent(1));
+
+								movecount++;
+
+								row = row + 1;
+								col = col - 1;
+
+								pawn.click = false;
+
+								p_board.getParent().validate();
+								p_board.getParent().repaint();
+
+							}
+						});
+
+					}
+				}
+			}
+			if (row + 1 <= 8 && col + 1 <= 8) {
+				// 오른쪽 대각선
+				if (boards[row + 1][col + 1].getComponentCount() == 2) {
+					if (((Pawn) boards[row + 1][col + 1].getComponent(1)).side.equals("white")) {
+
+						movepins[row + 1][col + 1].setVisible(true);
+						attack = true;
+
+						movepins[row + 1][col + 1].addActionListener(new ActionListener() {
+
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								boards[row][col].remove(pawn);
+
+								boards[row + 1][col + 1].add(pawn);
+								pawn.setBackground(Color.white);
+
+								boards[row + 1][col + 1].remove(boards[row + 1][col + 1].getComponent(1));
+
+								movecount++;
+
+								movepinsNotVisible(movepins);
+
+								row = row + 1;
+								col = col + 1;
+
+								pawn.click = false;
+
+								p_board.getParent().validate();
+								p_board.getParent().repaint();
+
+							}
+						});
+
+					}
+				}
+			}
+
+			if (boards[row + 1][col].getComponentCount() == 2 && !attack) {
 				return;
 			}
 
@@ -56,38 +135,39 @@ public class Pawn extends JButton {
 
 			}
 
-			removeAction(movepins);
+			if (boards[row + 1][col].getComponentCount() != 2) {
 
-			movepins[row + 1][col].setVisible(true);
-			this.setBackground(Color.red);
-			movepins[row + 1][col].addActionListener(new ActionListener() {
+				movepins[row + 1][col].setVisible(true);
+				this.setBackground(Color.red);
+				movepins[row + 1][col].addActionListener(new ActionListener() {
 
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					boards[row][col].remove(pawn);
-					boards[row + 1][col].add(pawn);
-					pawn.setBackground(Color.white);
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						boards[row][col].remove(pawn);
+						boards[row + 1][col].add(pawn);
+						pawn.setBackground(Color.white);
 
-					for (int i = 1; i <= 8; i++) {
-						for (int j = 1; j <= 8; j++) {
-							movepins[i][j].setVisible(false);
+						for (int i = 1; i <= 8; i++) {
+							for (int j = 1; j <= 8; j++) {
+								movepins[i][j].setVisible(false);
+							}
 						}
+
+						movepinsNotVisible(movepins);
+
+						System.out.println("1칸");
+
+						movecount++;
+
+						row = row + 1;
+
+						pawn.click = false;
+
+						p_board.getParent().validate();
+						p_board.getParent().repaint();
 					}
-
-					movepinsNotVisible(movepins);
-					
-					System.out.println("1칸");
-
-					movecount++;
-
-					row = row + 1;
-
-					pawn.click = false;
-
-					p_board.getParent().validate();
-					p_board.getParent().repaint();
-				}
-			});
+				});
+			}
 		}
 
 		// 앞으로 두칸
@@ -104,7 +184,7 @@ public class Pawn extends JButton {
 					pawn.setBackground(Color.white);
 
 					movepinsNotVisible(movepins);
-					
+
 					System.out.println("2칸");
 
 					movecount++;
@@ -118,79 +198,6 @@ public class Pawn extends JButton {
 
 				}
 			});
-		}
-
-		// 왼쪽 대각선
-		if (row + 1 <= 8 && col - 1 >= 1) {
-			if (boards[row + 1][col - 1].getComponentCount() == 2) {
-				if (((Pawn) boards[row + 1][col - 1].getComponent(1)).side.equals("white")) {
-
-					movepins[row + 1][col - 1].setVisible(true);
-
-					movepins[row + 1][col - 1].addActionListener(new ActionListener() {
-
-						@Override
-						public void actionPerformed(ActionEvent e) {
-							boards[row][col].remove(pawn);
-
-							boards[row + 1][col - 1].add(pawn);
-							pawn.setBackground(Color.white);
-
-							movepinsNotVisible(movepins);
-
-							boards[row + 1][col - 1].remove(boards[row + 1][col - 1].getComponent(1));
-
-							movecount++;
-
-							row = row + 1;
-							col = col - 1;
-
-							pawn.click = false;
-
-							p_board.getParent().validate();
-							p_board.getParent().repaint();
-
-						}
-					});
-
-				}
-			}
-		}
-		if (row + 1 <= 8 && col + 1 <= 8) {
-			// 오른쪽 대각선
-			if (boards[row + 1][col + 1].getComponentCount() == 2) {
-				if (((Pawn) boards[row + 1][col + 1].getComponent(1)).side.equals("white")) {
-
-					movepins[row + 1][col + 1].setVisible(true);
-
-					movepins[row + 1][col + 1].addActionListener(new ActionListener() {
-
-						@Override
-						public void actionPerformed(ActionEvent e) {
-							boards[row][col].remove(pawn);
-
-							boards[row + 1][col + 1].add(pawn);
-							pawn.setBackground(Color.white);
-
-							boards[row + 1][col + 1].remove(boards[row + 1][col + 1].getComponent(1));
-
-							movecount++;
-							
-							movepinsNotVisible(movepins);
-
-							row = row + 1;
-							col = col + 1;
-
-							pawn.click = false;
-
-							p_board.getParent().validate();
-							p_board.getParent().repaint();
-
-						}
-					});
-
-				}
-			}
 		}
 	}
 
