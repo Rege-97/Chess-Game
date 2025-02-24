520406782b854559e94850b23124b8a00ca90e89
Rege-97 <cogus1027@gmail.com>
2025-02-24 23:44:47 +0900

    🚚 Rename: 이그노어에 있는 체스보드파일 푸쉬

---
+++ chessGame/src/chessGame/ChessBoard.java
@@ -0,0 +1,145 @@
+package chessGame;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ChessBoard extends JFrame {
+	int row, col;
+	JPanel p_board;
+	JPanel boards[][];
+	JButton bt_pawn;
+	JButton movepins[][];
+	String turn;
+	ImageIcon movepin;
+	ArrayList<ChessPiece> chesspiece_black, chesspiece_white;
+
+	public ChessBoard() {
+		super("테스트");
+
+		this.setSize(800, 800);
+		this.setVisible(true);
+		this.setLayout(new BorderLayout());
+
+		// 화면 중앙 출력
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Dimension frameSize = getSize();
+		int x = (screenSize.width - frameSize.width) / 2;
+		int y = (screenSize.height - frameSize.height) / 2;
+		setLocation(x, y);
+
+		// 창 닫기
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// 초기 턴 블랙
+		turn = "black";
+
+		// 보드 세팅
+		p_board = new JPanel(new GridLayout(8, 8));
+		boardSet();
+		this.add(p_board, "Center");
+
+		// 말 배치
+		setChessPiece();
+		
+		// 블랙 체스말 이벤트
+		for (int i = 0; i < chesspiece_black.size(); i++) {
+			final int index = i;
+			chesspiece_black.get(index).addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (turn.equals("black")) {
+						chesspiece_black.get(index).blackMove(boards, movepins, p_board, chesspiece_black);
+					}
+				}
+			});
+		}
+		
+		// 화이트 체스말 이벤트
+		for (int i = 0; i < chesspiece_white.size(); i++) {
+			final int index = i;
+			chesspiece_white.get(index).addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					if (turn.equals("white")) {
+						chesspiece_white.get(index).blackMove(boards, movepins, p_board, chesspiece_white);
+					}
+				}
+			});
+		}
+
+		this.validate();
+	}
+
+	// 체스 보드 그리기;
+	public void boardSet() {
+
+		boards = new JPanel[9][9];
+		movepins = new JButton[9][9];
+		movepin = new ImageIcon("image/MovePoint.png");
+
+		for (int i = 1; i <= 8; i++) {
+			for (int j = 1; j <= 8; j++) {
+				boards[i][j] = new JPanel(new BorderLayout());
+				if (i % 2 == 0) {
+					if (j % 2 == 0) {
+						boards[i][j].setBackground(Color.white);
+					} else {
+						boards[i][j].setBackground(Color.darkGray);
+					}
+				} else {
+					if (j % 2 == 0) {
+						boards[i][j].setBackground(Color.darkGray);
+					} else {
+						boards[i][j].setBackground(Color.white);
+					}
+				}
+				movepins[i][j] = new JButton();
+				movepins[i][j].setContentAreaFilled(false);
+				movepins[i][j].setFocusPainted(false);
+				movepins[i][j].setOpaque(false);
+				movepins[i][j].setIcon(movepin);
+				boards[i][j].add(movepins[i][j], "Center");
+				movepins[i][j].setVisible(false);
+				p_board.add(boards[i][j]);
+			}
+		}
+	}
+
+	// 체스 말 그리기
+	public void setChessPiece() {
+		chesspiece_black = new ArrayList<ChessPiece>();
+		chesspiece_white = new ArrayList<ChessPiece>();
+
+		chesspiece_black.add(new Rook("black", 1, 1, this));
+		chesspiece_black.add(new Rook("black", 1, 8, this));
+
+		boards[1][1].add(chesspiece_black.get(0), "Center");
+		boards[1][8].add(chesspiece_black.get(1), "Center");
+
+		chesspiece_white.add(new Rook("white", 8, 1, this));
+		chesspiece_white.add(new Rook("white", 8, 8, this));
+
+		boards[8][1].add(chesspiece_white.get(0), "Center");
+		boards[8][8].add(chesspiece_white.get(1), "Center");
+	}
+
+	public static void main(String[] args) {
+		System.setProperty("sun.java2d.uiScale", "1");
+		new ChessBoard();
+
+	}
+
+}
